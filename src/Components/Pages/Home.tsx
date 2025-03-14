d6f7c0e061c53b0841ed5469abfee4ed7d9c4b47
S-Rashy <sadiqrashidah@gmail.com>
2025-03-14 21:44:46 +0100

    feat: added player

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -5,19 +5,12 @@ import Trending from "../Trending/Trending";
 import Section2 from "../Section2/Section2";
 import Never from "../Never/Never";
 import Footer from "../../Footer/Footer";
-// import Player from "../Player/Player";
 import Testimonials from "../Test/Testimonial";
-// import Play from "../Player/play";
-// import Slider from "./Components/sample";
-// import CountdownTimer from "./Countdown";
+
 
 const Home = () => {
-  return ( <div>
-    {/* <Slider/> */}
-    {/* <Play/> */}
-    {/* <Player/> */}
+  return ( <div>   
     <Header/>
-    {/* <CountdownTimer/> */}
     <Hero/>
     <Trending/>
     <Section2/>
